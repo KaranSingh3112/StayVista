804d39b24d611f14ca6a2e751408d9202241cd7f
KaranSingh3112 <karansingh311206@gmail.com>
2026-05-08 19:44:52 +0530

    Restyled ratings added start icons

--- routes/listing.js
+++ routes/listing.js
@@ -4,10 +4,15 @@ const wrapAsync = require("../utils/wrapAsync");
 const { isLoggedin, isOwner, validateListing } = require("../middlewares.js");
 
 const listingController = require("../controllers/listings.js")
+const multer = require("multer")
+const upload = multer({dest: "uploads/"})
 
 router.route("/")
     .get(wrapAsync(listingController.index))
-    .post(isLoggedin, validateListing, wrapAsync(listingController.newListing))
+    // .post(isLoggedin, validateListing, wrapAsync(listingController.newListing))
+    .post(upload.single('listing[image]'),(req,res)=>{
+        res.send(req.file)
+    })
 
 
 //New Listing Form
