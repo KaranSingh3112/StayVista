713379302384e3eb21d1cb54583549d5a326f64d
KaranSingh3112 <karansingh311206@gmail.com>
2026-05-15 11:01:04 +0530

    Final push

--- app.js
+++ app.js
@@ -80,6 +80,10 @@ app.use((req,res,next)=>{
     next();
 })
 
+app.get("/",(req,res)=>{
+    res.redirect("/signup")
+})
+
 app.use("/listings",listingRouter)
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
